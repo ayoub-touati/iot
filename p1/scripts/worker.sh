4fe81222c3f339ba81cc4a2c7f0f1c3e4e70539c
atouati <ayoub.toouati@gmail.com>
2025-04-12 15:25:56 +0100

    fix(k3s): add the internal network ip to the nodes

--- p1/scripts/worker.sh
+++ p1/scripts/worker.sh
@@ -2,4 +2,5 @@ sudo apt-get update -y
 sudo apt-get install -y curl
 until [ -f /vagrant/k3s-token.txt ]; do sleep 1; done
 K3S_TOKEN=$(grep "K3S_TOKEN" /vagrant/k3s-token.txt | cut -d " " -f 2)
+export INSTALL_K3S_EXEC="agent --node-ip=192.168.56.111"
 sudo curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.110:6443 K3S_TOKEN=$K3S_TOKEN sh -
--- p2/scripts/k3s.sh
+++ p2/scripts/k3s.sh
@@ -1,4 +1,5 @@
 echo "192.168.56.110 app1.com app2.com app3.com" >> /etc/hosts
 apt-get update -y
 apt-get install -y curl
+export INSTALL_K3S_EXEC="server --node-ip=192.168.56.110"
 curl -sfL https://get.k3s.io | sh -s - --node-external-ip=192.168.56.110
