0b9e5c7ac3e5b47febe37c17562fbc0f19c678df
atouati <ayoub.toouati@gmail.com>
2025-04-13 03:12:28 +0100

    refactor(k3s): add some optimizations

--- p1/scripts/server.sh
+++ p1/scripts/server.sh
@@ -1,7 +1,6 @@
 sudo apt-get update -y
 sudo apt-get install -y curl
-export INSTALL_K3S_EXEC="server --node-ip=192.168.56.110"
-sudo curl -sfL https://get.k3s.io | sh -
+sudo curl -sfL https://get.k3s.io | sh -s - server --node-ip=192.168.56.110
 echo "K3S_TOKEN: $(sudo cat /var/lib/rancher/k3s/server/node-token)" > /vagrant/k3s-token.txt
 echo "alias k='sudo kubectl'" >> .bashrc
 source .bashrc
--- p2/scripts/k3s.sh
+++ p2/scripts/k3s.sh
@@ -1,5 +1,5 @@
+# Add hostnames to /etc/hosts
 echo "192.168.56.110 app1.com app2.com app3.com" >> /etc/hosts
 apt-get update -y
 apt-get install -y curl
-export INSTALL_K3S_EXEC="server --node-ip=192.168.56.110"
-curl -sfL https://get.k3s.io | sh -s - --node-external-ip=192.168.56.110
+curl -sfL https://get.k3s.io | sh -s - server --node-ip=192.168.56.110
