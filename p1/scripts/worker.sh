776557ec37dbf14158a475aed4e550ceed6a02c4
atouati <ayoub.toouati@gmail.com>
2025-04-13 03:12:28 +0100

    refactor(k3s): add some optimizations

--- p1/scripts/worker.sh
+++ p1/scripts/worker.sh
@@ -2,5 +2,4 @@ sudo apt-get update -y
 sudo apt-get install -y curl
 until [ -f /vagrant/k3s-token.txt ]; do sleep 1; done
 K3S_TOKEN=$(grep "K3S_TOKEN" /vagrant/k3s-token.txt | cut -d " " -f 2)
-export INSTALL_K3S_EXEC="agent --node-ip=192.168.56.111"
-sudo curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.110:6443 K3S_TOKEN=$K3S_TOKEN sh -
+sudo curl -sfL https://get.k3s.io | sh -s - agent --server=https://192.168.56.110:6443 --token=$K3S_TOKEN --node-ip=192.168.56.111
