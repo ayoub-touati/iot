cd44363188e2aaac2098599ac8f7e0d0c535a3c1
atouati <ayoub.toouati@gmail.com>
2025-04-13 15:37:40 +0100

    refactor(k3s): rename the name of conf folder to confs

--- bonus/scripts/argocd.sh
+++ bonus/scripts/argocd.sh
@@ -3,7 +3,7 @@ DIR=$PWD
 # #NOTE: deploying argocd
 
 #ISSUE: pottential issue: we might need to wait for gitlab to be ready before deploying argocd??
-sudo kubectl apply -f ${DIR}/conf/argocd-conf.yml
+sudo kubectl apply -f ${DIR}/confs/argocd-conf.yml
 sudo kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 # #NOTE: deploying application ressource with argocd which would automate wil42 deployment and CI
-sudo kubectl apply -f ${DIR}/conf/desired-state.yml
+sudo kubectl apply -f ${DIR}/confs/desired-state.yml
